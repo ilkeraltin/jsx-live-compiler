8207fc3fa2ea27a624ce84818874f7de14a5e8b9
ilker ALTIN <ilkerobi@gmail.com>
2016-01-15 13:53:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '/* add jsx here */',
+      output: '',
+      err: ''  
+    }
+    this.update = this.update.bind(this)
+  }
+  update(e){
+    let code = e.target.value
+    try {
+      this.setState({
+        output: babel.transform(code, {
+                    stage: 0,
+                    loose: "all"
+                }).code,
+        err: ''
+      })
+    }
+    catch(err){
+      this.setState({
+        err: err.message
+      })
+    }
+  }
+  render(){
+    return (
+      <div>
+        <header>&nbsp;{this.state.err}</header>
+        <div className="container">
+          <textarea onChange={this.update} defaultValue={this.state.input}>
+          </textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
